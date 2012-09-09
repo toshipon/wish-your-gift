038aecfc015cae7582e625c7b5daffbbe581118d
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2012-09-09 15:52:20 +0900

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishYourGift::Application.config.secret_token = '618664aa30fc2002296c77b4491c6bed5b26998c631d6880d115fffd2f5c3b2838c4cc91da2c9f6545fea1b1af77ef1d36e3d39650a717bdc0a3e7a83a3bc024'
