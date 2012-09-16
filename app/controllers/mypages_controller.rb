429e516df6a90ecb145efdf9a48909f2edf5ceb7
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2012-09-17 00:11:03 +0900

    show my wishlist

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,5 +1,29 @@
 class MypagesController < ApplicationController
+  require 'open-uri'
+  require 'net/http'
+
   def index
+    if current_user && current_user.wishlist.has_key?
+      @wish_products = []
+      contents = Net::HTTP.get_response(URI.parse("http://www.amazon.co.jp/registry/wishlist/#{current_user.wishlist.key}?layout=compact")).body
+      contents.scan(/(http:\/\/[^\/]*\/[a-zA-Z0-9?&-_=%\/]*\/dp\/([a-zA-Z0-9?&-_=%][^\/]*)\/[a-zA-Z0-9?&-_=%\/]*)/) do |matched|
+        begin
+          product = {}
+          product.store("url", matched[0] + "&tag=happy_birthday-22")
+          res = Amazon::Ecs.item_lookup(matched[1], {:country => 'jp', :response_group => 'Medium',})
+          unless res.has_error?
+            res.items.each do |item|
+              product.store("title",item.get("ItemAttributes/Title"))
+              product.store("image", item.get("MediumImage/URL"))
+              product.store("price", item.get("ItemAttributes/ListPrice/FormattedPrice"))
+            end
+          end
+          @wish_products << product
+        rescue => e
+          next
+        end
+      end
+    end
   end
 
   def register_wishlist_id
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def slice_by_length(str, str_length)
+    str.split(//).first(str_length).inject("") do |result, char|
+      result += char
+    end
+  end
 end
